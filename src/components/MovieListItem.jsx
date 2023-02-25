20472d8309043fe33a7dc1010c4e5f746ded76c8
Sean-Jamieson <71718653+Sean-Jamieson@users.noreply.github.com>
2023-02-24 23:50:37 -0700

    adjusted css for testing purposes

--- src/components/MovieListItem.jsx
+++ src/components/MovieListItem.jsx
@@ -6,7 +6,7 @@ const MovieListItem = (props) => {
 
     return (
         <li className="inline-grid grid-cols-4 ">
-            <div>{props.movie.title}</div><div>{props.movie.release_date}</div><div>{props.movie.ratings.average}</div><div>{props.movie.ratings.popularity}</div>
+            <div>{props.movie.title}</div><div>{props.movie.release_date.split("-")[0]}</div><div>{props.movie.ratings.average}</div><div>{props.movie.ratings.popularity}</div>
         </li>
     )
 
