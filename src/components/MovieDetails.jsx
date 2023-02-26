92b46dc4eb4dbd34330f05bc00291731f0c4106f
malwa355 <malwa355@mtroyal.ca>
2023-02-26 14:14:52 -0700

    basic details and ratings functionality. Included font-awesome

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Header from "./Header.jsx";
 import {Link} from "react-router-dom";
 import Rating from "./Rating.jsx";
+import Favorites from "./Favorites.jsx";
+import Panel from "./Panel.jsx";
 
 const MovieDetails = (props) => {
-
+    console.log(props.movie);
     const favoriteHandler = ()=>{
         props.addFavorite(props.movie.id);
     }
@@ -21,22 +23,25 @@ const MovieDetails = (props) => {
                         <p>Release Date: {props.movie.release_date}</p>
                         <p>Runtime: {props.movie.runtime}</p>
                         <p>Tagline: {props.movie.tagline}</p>
-                        <p>Revenue: {props.movie.revenue}</p>
+                        <p>Revenue: ${props.movie.revenue.toLocaleString()}</p>
                         <a href={`https://www.imdb.com/title/${props.movie.imdb_id}`}>IMDB page for {props.movie.title}</a>
                         <a href={`https://www.themoviedb.org/movie/${props.movie.tmdb_id}`}>TMDB page for {props.movie.title}</a>
                         {console.log(props.movie)}
                         <p>Overview: {props.movie.details.overview}</p>
                         <p>Genre(s):</p> <ul>{props.movie.details.genres.map( (g,index) => <li key={index}>{g.name}</li> )}</ul>
                     </div>
-                    <Rating></Rating>
+                    <Rating movie={props.movie}></Rating>
                 </div>
                 <div>
                     <Link to="/movies"> 
                         <button>Cancel</button>
                     </Link>
-                    <button onClick={favoriteHandler}>❤ Add to Favs</button>
+                    <button onClick={favoriteHandler}><i className="fa-solid fa-heart" /*style={{color:"pink"}}*/></i> Add to Favs</button>
                 </div>
             </div>
+            <Panel id="favorites">
+                <Favorites favorites={props.favorites}></Favorites>
+            </Panel>
         </div>
     )
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,9 +3,30 @@ import React from "react";
 
 
 const Rating = (props) => {
-
+    const fullStarCount = props.movie.ratings.average.toFixed(0);
+    const arr = new Array(10).fill(null);
+    let rounded = props.movie.ratings.average;
+    //cite stack overflow for this method vvvv!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    rounded = (Math.round(rounded * 2) / 2).toFixed(1)
+    arr.fill(1,0,fullStarCount);
+    let halfStar = rounded%1;
+    arr[fullStarCount-1] = halfStar;
+    console.log(props.movie.ratings.average);
+    console.log(arr);
     return (
-        null
+        <div>
+            <p>Current Interest: {props.movie.ratings.popularity.toFixed(2)}</p>
+            <p>Current Average Rating: {props.movie.ratings.average} {
+                arr.map((s,i)=>{
+                    if(s===1){return(<i className="fa-solid fa-star" key={i} style={{color:"orange"}}></i>)}
+                    else if(s===0.5){return(<i className="fa-solid fa-star-half-stroke" key={i} style={{color:"orange"}}></i>)}
+                    else {return(<i className="fa-regular fa-star" key={i} style={{color:"orange"}}></i>)}
+                })
+            }</p>
+            <p>Number of Ratings: {props.movie.ratings.count}</p>
+            {/* <Rater rateMovie={props.rateMovie}></Rater> */}
+        </div>
+
     )
 
 }
